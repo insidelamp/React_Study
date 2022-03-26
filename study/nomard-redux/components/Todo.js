b2d1499c5bb350d4f572fda6ec3ca6bbe3afe0fc
insidelamp <rkdhfh@naver.com>
2022-03-26 20:57:59 +0900

    노마드 리덕스

---
+++ study/nomard-redux/components/Todo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
+
+function ToDo({ text, onBtnClick, id }) {
+  return (
+    <li>
+      <Link to={`/${id}`}>
+        {text} <button onClick={onBtnClick}>DEL</button>
+      </Link>
+    </li>
+  );
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(ToDo);
